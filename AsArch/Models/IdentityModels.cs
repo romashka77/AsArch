75137c25dae9e10d3174ab75a5fa4f8a91494c6b
romashka77 <romashka_77@mail.ru>
2018-09-27 13:21:32 +0300

    Проверка "Identity"

--- AsArch/Models/IdentityModels.cs
+++ AsArch/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace AsArch.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Identity", throwIfV1Schema: false)
         {
         }
 
